1a6e63bdaed5abdd24d2f67787aa7cc022861e77
xsotopi <xaviminisoto@gmail.com>
2024-04-18 11:04:46 +0200

    Improved app design, only left add inferences

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, request, send_file, send_from_directory
+from flask import Flask, render_template, request, send_file, send_from_directory, flash, redirect, url_for
+from werkzeug.utils import secure_filename
 import os
+
 app = Flask(__name__)
 
 UPLOAD_FOLDER = 'uploads'
@@ -8,38 +10,46 @@ ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
+@app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return 'No file part'
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file'
-    if file and allowed_file(file.filename):
-        filename = file.filename
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        
-        # Perform inference of models here, can save and preview the both files
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part', 'error')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file', 'error')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            return render_template('preview.html', filename=filename)
+    return render_template('upload.html')
 
-        file.save(file_path)
-        return render_template('preview.html', filename=filename)
+@app.route('/contact')
+def contact_us():
+    return render_template('contact.html')
 
-@app.route('/preview/<filename>')
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/preview/<filename>', methods=['GET'])
 def preview_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-@app.route('/download/<filename>')
+@app.route('/download/<filename>', methods=['GET'])
 def download_file(filename):
     return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), as_attachment=True)
 
-@app.route('/uploads/<filename>')
+@app.route('/uploads/<filename>', methods=['GET'])
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
